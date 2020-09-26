0c57ff0cd621e931744866a9b8050879b6ca89d1
skrypkasv <skrypkasv@gmail.com>
2020-09-26 21:12:21 +0300

    Fixes mod_6/task_09

--- module6/task_09.js
+++ module6/task_09.js
@@ -1,8 +1,34 @@
-// Write code under this line
-const getNamesSortedByFriendsCount = (array) =>
-  [...array]
-    .sort((a, b) => a.friends.length - b.friends.length)
-    .map(({ name }) => name);
+//sort и map
+
+// Получи массив имен (поле name) людей,
+// отсортированных в зависимости от количества их
+// друзей(поле friends)
+
+// Избегаем мутации исходного массива: т.к. метод
+// sort изменяет(мутирует) исходный массив, то
+// следует сделать копию массива и сортировать
+// уже копию, а не исходный массив.
+
+// Копирование массива:
+/*const arr = [1, 3, 5];
+
+// 1
+const first = [...arr];
+
+// 2
+const second = arr.slice();
+
+// 3
+const third = arr.concat();*/
+
+// Используй деструктурирующее присваивание для
+// параметра функции({name}) без пробелов и
+// переносов на новую строку
+
+// Используй только перебирающие методы массива
+// которые не изменяют(не мутируют) исходный
+// массив.Т.е.нельзя использовать for, splice,
+// push и т.п.мутирующие методы.
 
 const users = [
   {
@@ -90,6 +116,20 @@ const users = [
     age: 39,
   },
 ];
+
 console.log(getNamesSortedByFriendsCount(users));
 // [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
-// console.log(users);
+console.log(users);
+
+//
+//
+//
+//
+//
+//
+//
+//
+// const getNamesSortedByFriendsCount = (array) =>
+//   [...array]
+//     .sort((objA, objB) => objA.friends.length - objB.friends.length)
+//     .map(({ name }) => name);
